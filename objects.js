31a3d77977ee5a752163b190d300f2a0123466a4
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-15 18:47:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,15 @@
 var playlist = {
 'artist': 'song'
 }
+
+function updatePlaylist(obj, artist, song) {
+  //add the song and artist as key value
+  //return whole playlist
+  var updatedPlaylist = {
+    artist: song
+  }
+
+  Object.assign(obj, updatedPlaylist);
+  
+  return updatedPlaylist;
+}
